43cb2bb23d6dd77a20d86c8c33c2bfbfad454147
RedBullDogE <gsb06092000@gmail.com>
2021-04-12 15:15:21 +0300

    Implement Message CRUD

--- api/views.py
+++ api/views.py
@@ -1,4 +1,17 @@
-from django.shortcuts import render
+from django.core.paginator import Paginator, EmptyPage
+from rest_framework.response import Response
+from rest_framework.serializers import Serializer
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from rest_framework.pagination import PageNumberPagination
 
+from .models import Message
+from .serializers import MessageSerializer
+
+
+class MessageViewSet(ModelViewSet):
+    """
+    A viewset for viewing and editing user instances.
+    """
+    serializer_class = MessageSerializer
+    queryset = Message.objects.all()
--- cute_chat/settings.py
+++ cute_chat/settings.py
@@ -39,7 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
-    'drf_yasg'
+    'drf_yasg',
+
+    'api'
 ]
 
 MIDDLEWARE = [
@@ -130,3 +132,9 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# DRF settings
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}

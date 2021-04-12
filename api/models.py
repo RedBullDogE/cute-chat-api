4419a6cbae135d8d9d4331c2b4e4f53e168d6231
RedBullDogE <gsb06092000@gmail.com>
2021-04-12 14:04:38 +0300

    Initial project setup

---
+++ api/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.core.exceptions import ValidationError
+
+
+class Message(models.Model):
+    author_email = models.EmailField()
+    text = models.CharField(max_length=100, blank=False)
+    create_date = models.DateTimeField(auto_now_add=True)
+    update_date = models.DateTimeField(auto_now=True)
+
+    def clean(self):
+        if self.text == '':
+            raise ValidationError('Message text should not be empty')
